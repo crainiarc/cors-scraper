379b39b50ebd96ae79ea6f0ed5ae672376c56d5f
Keng Kiat Lim <kkips@crnrc.com>
2012-07-04 02:05:47 +0800

    Updated spider to work with the new CORS format
    CORS recently updated their ModuleDetailedInfo page and made it more
    consistent.
    
    Changes:
    - Spider now works with this new format
    - Due to the consistency, we can now group lectures and tutorials under
    a single item known as slots.
    - MySQL pipeline has been refactored
    - Scrapper now queries and adds in new slot types.

--- cors/SanitizeDataPipeline.py
+++ cors/SanitizeDataPipeline.py
@@ -10,116 +10,22 @@ class SanitizeDataPipeline(object):
         item['prereq'] = self._clean(item['prereq']) if item['prereq'] else None
         item['preclu'] = self._clean(item['preclu']) if item['preclu'] else None
         item['workload'] = self._clean(item['workload']) if item['workload'] else None
-        
-        # Reformat lectures and tutorials strings
-        item['lectures']  = [lecture.strip() for lecture in item['lectures']]
-        item['lectures']  = self._timeParse(item['lectures']) if item['lectures'] else None
-        
-        item['tutorials'] = [tutorial.strip() for tutorial in item['tutorials']]
-        item['tutorials'] = self._timeParse(item['tutorials']) if item['tutorials'] else None
+        item['slots']  = self._parseSlots(item['slots']) if item['slots'] else None
         
         return item
     
-    def _timeParse(self, parse_list):
-        """
-    	This takes a list of parsed strings, like:
-
-    		[u'DESIGN LECTURE Class [1]',
-    		u'MONDAY From 1400 hrs to 1800 hrs in AKI2,',
-    		u'Week(s): EVERY WEEK.',
-    		u'THURSDAY From 1400 hrs to 1800 hrs in AKI2,',
-    		u'Week(s): EVERY WEEK.',
-    		u'Not Available in Tutorial Balloting yet.'],
-
-    	and turns it into this:
-
-    		[
-    			{
-    			'name': DESIGN LECTURE Class [1],
-    			'sessions':
-    					[{
-    						'day': 1,
-    						'starttime': '1400',
-    						'endtime': '1800',
-    						'location': 'AK12',
-    						'occurence': [1,2,3,4,5,6,7,8,9,10,11,12,13]
-    					}]
-    			}
-    		]
-
-    		There can be many lecture classes in the list. There can also be
-    		multiple sessions, which explains why the {day, starttime, endtime etc}
-    		dict is stored in a list.
-    	"""
-    	
-    	time = re.compile('(?P<day>\w+) From (?P<starttime>\d+) hrs to (?P<endtime>\d+) hrs in (?P<location>.+),')
-    	occur = re.compile('Week\(s\): (.*?)\.')
-    	ballot = re.compile('.*? Tutorial Balloting .*?')
-    	nolecture = re.compile('.*? no lectures .*?')
-
-    	res = []
-    	pos = 0 # pos indicates tutorial or lecture position in the list
-    	secondary = 0 # secondary indicates session number, for classes with multiple sessions
-
-    	for l in parse_list:
-    		time_re = time.match(l)
-    		occur_re = occur.match(l)
-    		ballot_re = ballot.match(l)
-
-    		# Check if there are no lectures.
-    		if nolecture.match(l):
-    		    return None
-
-    		if not time_re and not occur_re and not ballot_re:
-    			# If there are no matches in the above regular expressions,
-    			# that can only mean that it is the beginning of the new slot
-    			# details, or the end of the list.
-    			
-    			# If res is empty and position is 0, assign the first line in
-    			# the list to be the name.
-    			if pos == 0 and not res:
-    				res.append({'name': l})
-    				
-    			# If res is already populated and we know l exists
-    			# then we know this is the next lesson slot.
-    			elif l:
-    				res.append({'name': l})
-    				secondary = 0
-    				pos += 1
-    				
-    			# otherwise, bugfix for added table that causes name to be empty.
-    			else:
-    				pass
-    				
-            # If the current line states the time...
-    		if time_re:
-    			day = time_re.group('day')
-    			starttime = time_re.group('starttime')
-    			endtime = time_re.group('endtime')
-    			location = time_re.group('location')
-
-    			curr_session = {
-    			    'day': self._convertDay(day),
-    				'starttime': starttime,
-    				'endtime': endtime,
-    				'location': self._clean(location)
-    			}
-    			
-    			# If this is the first session
-    			if secondary == 0:
-    				res[pos]['sessions'] = [curr_session]
-    			else:
-    				res[pos]['sessions'].append(curr_session)
-
-    		if occur_re: # If the current line states the occurrence
-    			occurence = self._convertOccur(occur_re.group(1))
-    			res[pos]['sessions'][secondary]['occurrence'] = occurence
-    			
-    			# Next line is read is either another session, or a new slot
-    			# We increment the counter just in case...
-    			secondary += 1
-    			
-    	return res
+    def _parseSlots(self, slots):
+        """Cleans up the slot dict to a more database-friendly format"""
+        
+        for slot in slots:
+            slot["code"] = ' '.join(slot["code"].split()) if slot["code"] else None
+            slot["type"] = self._clean(slot["type"])
+            slot["day"]  = self._convertDay(slot["day"])
+            
+            slot["occurrence"] = self._clean(slot["occurrence"])
+            slot["occurrence"] = self._convertOccur(slot["occurrence"])
+        
+        return slots
     
     def _convertDay(self, day):
     	"""Takes day as scraped, and converts it to number representations"""
